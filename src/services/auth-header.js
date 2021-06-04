21c71bf85964ac120168698fdfd95b1492a3f41b
Muhamad Rian Alpajirin <ryan.cyborg205@gmail.com>
2021-06-04 19:03:24 +0700

    implement permission

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,6 +1,5 @@
 export default function authHeader() {
     let user = JSON.parse(localStorage.getItem('user'));
-    console.log(user)
 
     if (user && user.token) {
         return { Authorization: 'Token ' + user.token };
